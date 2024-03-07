04a262dff1208b48428fc2f1477dab117d3c824d
Ayaz Ahmad <iamayyadahmad@gmail.com>
2024-03-07 23:55:33 +0500

    modified js

--- js/main.js
+++ js/main.js
@@ -51,12 +51,66 @@ function saveBookmark(e) {
     e.preventDefault();
 }
 
+//Delete Bookmark
+function deleteBookmark(url) {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+    for (i = 0; i < bookmarks.length; i++) {
+        if (bookmarks[i].url == url) {
+            bookmarks.splice(i, 1)
+        }
+    }
+
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+
+    fetchBookmarks()
+}
+
+
 //fetch Bookmarks
 
 function fetchBookmarks() {
-    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
-    console.log(bookmarks)
+    // Get bookmarks from localStorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // Get output id
+    var bookmarksResults = document.getElementById('bookmarksResults');
 
-    var bookmarksResults = document.getElementById('bookmarksResults')
-    bookmarksResults.innerHTML = 'Hello'
+    // Build output
+    bookmarksResults.innerHTML = '';
+    for (var i = 0; i < bookmarks.length; i++) {
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+
+        bookmarksResults.innerHTML += '<div class="well">' +
+            '<h3>' + name +
+            ' <a class="btn btn-default" target="_blank" href="' + addhttp(url) + '">Visit</a> ' +
+            ' <a onclick="deleteBookmark(\'' + url + '\')" class="btn btn-danger" href="#">Delete</a> ' +
+            '</h3>' +
+            '</div>';
+    }
+}
+
+// Validate Form
+function validateForm(siteName, siteUrl) {
+    if (!siteName || !siteUrl) {
+        alert('Please fill in the form');
+        return false;
+    }
+
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+
+    if (!siteUrl.match(regex)) {
+        alert('Please use a valid URL');
+        return false;
+    }
+
+    return true;
+}
+
+function addhttp(url) {
+    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+        url = "http://" + url;
+    }
+    return url;
 }
